07bbd6ed0aba14db1676a18b930d0012c0ddf0ad
Ronny Trommer <ronny@opennms.org>
2023-09-04 19:41:26 +0200

    fix: Install Ansible community collection for PostgreSQL

--- scripts/041-ansible.sh
+++ scripts/041-ansible.sh
@@ -1,2 +1,6 @@
 #!/bin/sh -eux
+apt-get install -y python3-pip
+# Workaround for issue https://github.com/ansible-collections/community.digitalocean/issues/132
+pip install -Iv 'resolvelib<0.6.0'
 apt-get install -y acl ansible-core
+ansible-galaxy collection install community.postgresql
