7b3e7fe6e28643cb419fbd732e4b30ca9f39fa46
Ronny Trommer <ronny@opennms.org>
2023-09-05 12:58:33 +0200

    fix: Install Ansible from Ansible repositories in a current version with community collections

--- scripts/041-ansible.sh
+++ scripts/041-ansible.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -eux
-apt-get install -y python3-pip
-# Workaround for issue https://github.com/ansible-collections/community.digitalocean/issues/132
-pip install -Iv 'resolvelib<0.6.0'
-apt-get install -y acl ansible-core
+apt-get install -y software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt-get install -y ansible
